829f7c3cf9af02bab4fc33efcb03e0b176a68eee
Muhammad Saad <msaadasif06@gmail.com>
2026-05-10 03:00:59 +0500

    Saad

--- ChessGame/Board.cpp
+++ ChessGame/Board.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include "Board.h"
+#include "Rook.h"
+#include "Knight.h"
+#include "Bishop.h"
+#include "Queen.h"
+#include "King.h"
+#include "Pawn.h"
 using namespace std;
 
 //initialize empty board
@@ -12,6 +18,36 @@ Board::Board()
 			grid[i][j] = nullptr;
 		}
 	}
+	//Black Pieces (false)	
+	grid[0][0] = new Rook(false);
+	grid[0][1] = new Knight(false);
+	grid[0][2] = new Bishop(false);
+	grid[0][3] = new Queen(false);
+	grid[0][4] = new King(false);
+	grid[0][5] = new Bishop(false);
+	grid[0][6] = new Knight(false);
+	grid[0][7] = new Rook(false);
+
+	for (int i = 0; i < 8; i++)
+	{
+		grid[1][i] = new Pawn(false);
+	}
+	
+	//White Pieces (true)
+	for (int i = 0; i < 8; i++)
+	{
+		grid[6][i] = new Pawn(true);
+	}
+
+	grid[7][0] = new Rook(true);
+	grid[7][1] = new Knight(true);
+	grid[7][2] = new Bishop(true);
+	grid[7][3] = new Queen(true);
+	grid[7][4] = new King(true);
+	grid[7][5] = new Bishop(true);
+	grid[7][6] = new Knight(true);
+	grid[7][7] = new Rook(true);
+}
 }
 
 //cleanup memory
@@ -64,16 +100,20 @@ bool Board::movePiece(int startX, int startY, int endX, int endY, bool isWhiteTu
 		cout << "not your turn" << endl;
 		return false;
 	}
+
+	// Ask Member 2 if the math/geometry is valid
 	if (grid[startY][startX]->isValidMove(startX, startY, endX, endY, grid))
 	{
+		// Basic capture before Member 3's advanced logic
 		if (grid[endY][endX] != nullptr)
 		{
 			delete grid[endY][endX];
 		}
+
 		grid[endY][endX] = grid[startY][startX];
 		grid[startY][startX] = nullptr;
 		return true;
 	}
+
 	cout << "invalid move" << endl;
 	return false;
-}
