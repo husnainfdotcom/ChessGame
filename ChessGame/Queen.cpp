f7931db2483e80d5903e9bcf9f4f22a0e6cce06d
Husnain <Hunain@DELL>
2026-05-10 15:57:17 +0500

    Resolved a moving logic bug in Queen piece.

--- ChessGame/Queen.cpp
+++ ChessGame/Queen.cpp
@@ -1,9 +1,9 @@
 #include "Queen.h"
 #include<cmath>
 
-Queen::Queen(char c) :Piece(c, 'Q') {};
+Queen::Queen(bool isWhite) :Piece(isWhite, 'Q') {};
 
-bool Queen::isValidMove(int startX, int startY, int endX, int endY) {
+bool Queen::isValidMove(int startX, int startY, int endX, int endY, Piece* board[8][8]) {
 	int xdiff = abs(startX - endX);
 	int ydiff = abs(startY - endY);
 	if (startX == endX && startY == endY) {
